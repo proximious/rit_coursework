99f9e0df6970fc147fddc5f2e5a1fc47d6d673f1
alex <alexiacob28@gmail.com>
2024-09-26 15:13:27 -0400

    added fourth year changes part 2

---
+++ year4/CSCI251/inClass/Module 042/04-02/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+
+    public class Program
+    {
+  
+        public static void Main(string[] args)
+        {
+
+        
+            var tasks = new List<Task>();
+            var n = Int32.Parse(args[0]);
+            var shared = new sharedNumber(0);
+            Console.WriteLine(shared.Val);
+            var cu = new CountUp();
+            var cd = new CountDown();
+            tasks.Add(Task.Run(()=>cu.go(shared, n)));
+            tasks.Add(Task.Run(()=>cd.go(shared, n)));
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine(shared.Val);
+
+        }
+    }
+
+    public class sharedNumber
+    {
+        private int val;
+        public static Object myLock = new Object();
+
+        public sharedNumber(int _val)
+        {
+            val = _val;
+        }
+        public void up()
+        {
+            try
+            {
+                Monitor.Enter(this);
+                val += 1;
+            }
+            finally
+            {
+
+                Monitor.Exit(this);
+            }
+
+            lock (myLock)
+            {
+                val += 1;
+            }
+
+
+        }
+
+        public void down()
+        {
+            
+            lock(myLock) {
+                val -= 1;
+             
+            }
+            
+        }
+
+        public int Val => val;
+    }
+
+    public class CountUp
+    {
+        public void go(sharedNumber shared, int n)
+        {
+            for (int i=0; i<n; i++)
+            {
+                shared.up();
+                
+            }
+        }  
+    }
+
+    public class CountDown
+    {
+        public void go(sharedNumber shared, int n)
+        {
+            for (int i=0; i<n; i++)
+            {
+                shared.down();
+            }
+        }  
+    }
+}
